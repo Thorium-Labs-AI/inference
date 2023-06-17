7418c801c990016a414d8f8818b431593f5ab632
Kristóf Váradi <vkristof20@protonmail.com>
2023-06-17 11:13:27 +0200

    removed profile from aws tf provider

--- ci/variables.tf
+++ ci/variables.tf
@@ -3,15 +3,11 @@ variable AWS_SECRET_ACCESS_KEY {
 }
 
 variable project_name {
-  default = "github-action-ecr"
+  default = "chat-server"
 }
 
 variable region {
   default = "us-east-1"
-}
-
-variable profile {
-  default = "default"
 }
 
 variable ecr_image {
