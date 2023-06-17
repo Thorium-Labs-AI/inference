cec3eaf4c455b0e7c88a3cdec175bc388b396f86
Kristóf Váradi <vkristof20@protonmail.com>
2023-06-17 10:56:36 +0200

    switched to terraform

---
+++ ci/main.tf
@@ -0,0 +1,25 @@
+provider aws {
+  region  = var.region
+  profile = var.profile
+}
+
+terraform {
+  cloud {
+    organization = "AsteriskChat"
+
+    workspaces {
+      name = "chat-server"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
---
+++ ci/variables.tf
@@ -0,0 +1,19 @@
+variable project_name {
+  default = "github-action-ecr"
+}
+
+variable region {
+  default = "us-east-1"
+}
+
+variable profile {
+  default = "default"
+}
+
+variable ecr_image {
+  default = ""
+}
+
+variable desired_count {
+  default = 1
+}
