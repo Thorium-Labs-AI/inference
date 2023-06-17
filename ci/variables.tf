7110320cc9a2b22093eb5042ef37e83b0dac32a0
Kristóf Váradi <vkristof20@protonmail.com>
2023-06-17 11:57:15 +0200

    get ecr image from aws in deploy

--- ci/variables.tf
+++ ci/variables.tf
@@ -10,13 +10,8 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "ecr_repository" {
-  default = "883869506849.dkr.ecr.us-east-1.amazonaws.com/chat-server"
-}
-
 variable "ecr_image" {
   description = "The tag for the ECR image. Defaults to 'latest'."
-  default     = "latest"
 }
 
 variable "desired_count" {
