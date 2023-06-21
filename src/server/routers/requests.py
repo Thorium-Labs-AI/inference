04dfa74b3aac2cacc9b584a12c241812aa10ebb5
Kristóf Váradi <vkristof20@protonmail.com>
2023-06-21 05:48:24 +0200

    forgot to add requirements.txt �

--- src/server/routers/requests.py
+++ src/server/routers/requests.py
@@ -1,24 +1,17 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
 
-from src.service.chat_service import process_message, get_config_from_db
+from src.service.auth.authentication import authenticated
+from src.service.chat_service import process_message
 
 router = APIRouter()
+
 
 class ChatRequest(BaseModel):
     message: str
 
-
-@router.get("/")
-async def get_chatbot_config(chatbot_id: str):
-    chatbot_config = get_config_from_db(chatbot_id)
-    if not chatbot_config:
-        raise HTTPException(status_code=404, detail="Chatbot config not found.")
-    return chatbot_config
-
-
 @router.post("/message/", tags=["chatbot"])
-async def send_message_to_chatbot(request: ChatRequest):
+async def send_message_to_chatbot(request: ChatRequest, token: str = Depends(authenticated)):
     response = process_message(request.message)
     if response is None:
         raise HTTPException(status_code=404, detail="Chatbot could not retrieve a response.")
