abcc872a9f23d4383daaef5247cd839270f24eec
Kristóf Váradi <vkristof20@protonmail.com>
2023-07-10 21:30:18 +0200

    removed authentication from message endpoint

--- src/server/routers/requests.py
+++ src/server/routers/requests.py
@@ -11,7 +11,7 @@ class ChatRequest(BaseModel):
     message: str
 
 @router.post("/message/", tags=["chatbot"])
-async def send_message_to_chatbot(request: ChatRequest, token: str = Depends(authenticated)):
+async def send_message_to_chatbot(request: ChatRequest):
     response = process_message(request.message)
     if response is None:
         raise HTTPException(status_code=404, detail="Chatbot could not retrieve a response.")
