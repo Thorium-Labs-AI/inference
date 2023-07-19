2fcb4e232d3e92333254194e6320e6e1e04c4eed
Kristóf Váradi <vkristof20@protonmail.com>
2023-07-19 06:57:50 +0200

    added every subdomain of thorium to allowed origins

--- src/server/app.py
+++ src/server/app.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "thoriumlabs.ai"
+        "*.thoriumlabs.ai"
     ],
     allow_credentials=True,
     allow_methods=["*"],
