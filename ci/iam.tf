907803c20df8f58a890b2fc634e967cc8bd29f4f
Kristóf Váradi <vkristof20@protonmail.com>
2023-08-15 20:10:52 +0200

    re-added AmazonECSTaskExecutionRolePolicy

--- ci/iam.tf
+++ ci/iam.tf
@@ -71,3 +71,8 @@ resource "aws_iam_policy" "task_role_policy" {
   description = "A test policy"
   policy      = data.aws_iam_policy_document.ecs_task_role_policy.json
 }
+
+resource aws_iam_role_policy_attachment ecs_task_execution_attachment {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
