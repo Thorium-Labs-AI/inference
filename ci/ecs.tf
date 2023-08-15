fd77d6ec026c5b55db7597145400177f3a2bf0a9
Kristóf Váradi <vkristof20@protonmail.com>
2023-08-15 07:08:59 +0200

    changed IAM policies in pterraform

--- ci/ecs.tf
+++ ci/ecs.tf
@@ -35,7 +35,8 @@ resource aws_ecs_task_definition task_definition {
 }]
 DEFINITION
 
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = 256
--- ci/iam.tf
+++ ci/iam.tf
@@ -1,4 +1,40 @@
-data aws_iam_policy_document ecs_assume_role_policy {
+# ECS Execution Role
+resource aws_iam_role ecs_execution_role {
+  name               = "${var.project_name}-ecs-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
+}
+
+data aws_iam_policy_document ecs_execution_role_policy {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetDownloadUrlForLayer", "ecr:BatchGetImage"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+}
+
+resource aws_iam_policy ecs_execution_policy {
+  name   = "${var.project_name}-ecs-execution_policy"
+  role   = aws_iam_role.ecs_execution_role.id
+  policy = data.aws_iam_policy_document.ecs_execution_role_policy
+}
+
+# ECS Task Policy
+
+data aws_iam_policy_document assume_role_policy {
   statement {
     actions = ["sts:AssumeRole"]
     principals {
@@ -8,39 +44,36 @@ data aws_iam_policy_document ecs_assume_role_policy {
   }
 }
 
-resource aws_iam_role ecs_service_role {
-  name               = "${var.project_name}-ecs-service-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_assume_role_policy.json
-}
-
-resource aws_iam_role_policy_attachment ecs_service_attachment {
-  role       = aws_iam_role.ecs_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
-}
-
-#
-# ecs-task assume role policy
-#
-
-# trust relationships
-data aws_iam_policy_document ecs_task_assume_role_policy {
+data aws_iam_policy_document ecs_task_role_policy {
   statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
+    effect  = "Allow"
+    actions = [
+      "dynamodb:PutItem",
+      "dynamodb:GetItem",
+      "dynamodb:BatchGetItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:DescribeTable"
+    ]
+
+    resources = [*]
   }
 }
 
-resource aws_iam_role ecs_task_execution_role {
-  name               = "${var.project_name}-ecs-task-execution-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
+resource aws_iam_role ecs_task_role {
+  name               = "${var.project_name}-ecs-task-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+resource aws_iam_role_policy ecs_task_role_policy {
+  name   = "${var.project_name}-ecs-task-role-policy"
+  role   = aws_iam_role.ecs_task_role.id
+  policy = data.aws_iam_policy_document.ecs_task_role_policy.json
 }
 
 # attach service-role/AmazonECSTaskExecutionRolePolicy
 # Provides access to other AWS service resources that are required to run Amazon ECS tasks
 resource aws_iam_role_policy_attachment ecs_task_execution_attachment {
-  role       = aws_iam_role.ecs_task_execution_role.name
+  role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
