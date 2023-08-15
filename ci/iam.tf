9d15adde6f7dceb43a35c3b1c81359cdf269882f
Kristóf Váradi <vkristof20@protonmail.com>
2023-08-15 16:15:22 +0200

    removed .json extensions from policy references

--- ci/iam.tf
+++ ci/iam.tf
@@ -1,7 +1,7 @@
 # ECS Execution Role
 resource aws_iam_role ecs_execution_role {
   name               = "${var.project_name}-ecs-task-execution-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_execution_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.ecs_execution_role_policy
 }
 
 data aws_iam_policy_document ecs_execution_role_policy {
@@ -9,7 +9,7 @@ data aws_iam_policy_document ecs_execution_role_policy {
     actions = ["sts:AssumeRole"]
     principals {
       type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
+      identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
 
@@ -29,11 +29,11 @@ data aws_iam_policy_document ecs_execution_role_policy {
 resource aws_iam_role_policy ecs_execution_policy {
   name   = "${var.project_name}-ecs-execution_policy"
   role   = aws_iam_role.ecs_execution_role.id
-  policy = data.aws_iam_policy_document.ecs_execution_role_policy.json
+  policy = data.aws_iam_policy_document.ecs_execution_role_policy
 }
 
-# ECS Task Policy
 
+# ECS Task Policy
 data aws_iam_policy_document assume_role_policy {
   statement {
     actions = ["sts:AssumeRole"]
@@ -62,17 +62,15 @@ data aws_iam_policy_document ecs_task_role_policy {
 
 resource aws_iam_role ecs_task_role {
   name               = "${var.project_name}-ecs-task-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy
 }
 
 resource aws_iam_role_policy ecs_task_role_policy {
   name   = "${var.project_name}-ecs-task-role-policy"
   role   = aws_iam_role.ecs_task_role.id
-  policy = data.aws_iam_policy_document.ecs_task_role_policy.json
+  policy = data.aws_iam_policy_document.ecs_task_role_policy
 }
 
-# attach service-role/AmazonECSTaskExecutionRolePolicy
-# Provides access to other AWS service resources that are required to run Amazon ECS tasks
 resource aws_iam_role_policy_attachment ecs_task_execution_attachment {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
