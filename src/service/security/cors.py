f5010f35757d5b0b1894b1c42cc661027a2f0092
Kristóf Váradi <vkristof20@protonmail.com>
2023-08-20 14:46:33 +0200

    temporarily added wildcard to allowed origins

--- src/service/security/cors.py
+++ src/service/security/cors.py
@@ -1,6 +1,7 @@
 allowed_origins = [
-        "http://localhost:5173",
-        "https://*.thoriumlabs.ai",
-        "https://thoriumlabs.ai",
-        "https://www.thoriumlabs.ai",
-    ]
+    "http://localhost:5173",
+    "https://*.thoriumlabs.ai",
+    "https://thoriumlabs.ai",
+    "https://www.thoriumlabs.ai",
+    "*"
+]
